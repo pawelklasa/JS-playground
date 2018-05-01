6ff06320bc8eb2824e902ed869c063cf2348ad36
Pawel Klasa <pawel.klasa@gmail.com>
2018-05-01 10:43:46 +0100

    print name end

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
+// Person constructor
 function Person(firstName, secondName) {
   this.firstName = firstName;
   this.secondName = secondName;
 }
 
-Person.prototype.someMethod = function() {
+// Person prototype/method
+Person.prototype.addNames = function() {
   return `Welcome ${this.firstName} ${this.secondName}`;
 }
 
+// UI
 document.getElementById('form-group').addEventListener('submit', function(e) {
   const firstName = document.getElementById('name').value,
         secondName = document.getElementById('surname').value;
         
-
   const person = new Person(firstName, secondName);
 
+  // Create element
+  const createCard = document.createElement('card');
+  // Append Child
+  createCard.appendChild(document.createTextNode(person.addNames()));
+  // Add to card
+  const card = document.getElementById('card');
+  // Add class to card
+  createCard.className = 'card name';
+  // Add new card to main card
+  card.appendChild(createCard);
+  // Clear input
+  document.getElementById('name').value = '';
+  document.getElementById('surname').value = '';
+
+  console.log(createCard);
   console.log(person);
-  console.log(person.someMethod());
+  console.log(person.addNames());
 
   e.preventDefault();
 });
+
+// ........................
+
 
